7165c1aa68efa1138dbfa491f6a43e2c05a5559e
JudgeMcFudge <christian.carbajosa63@gmail.com>
2017-05-15 19:18:57 +0930

    Code Fair 2017
    Group 15

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/**
+* author: Group 15 [Jasvir & Christian]
+*/
+
+/* TOP NAVIGATION (Example obtained from:
+[https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav]) */
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* DROP DOWN FOR TOUCH DEVICES (Example obtained from:
+[https://coderwall.com/p/3uwgga/make-css-dropdown-menus-work-on-touch-devices]) */
+
+//Allow clicking instead of hover for dropdown menus
+
+//Slideshow homepage
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); 
+}
+
+//Countdown timer
+var countDownDate = new Date("nov 10, 2017 11:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
